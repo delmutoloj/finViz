18dcae0ff9f8dc10a07074f30dc8e64b3248ba7d
delmutoloj <delmutoloj@usf.edu>
2025-03-23 20:11:29 -0400

    initial commit

---
+++ R/data-deposits.R
@@ -0,0 +1,13 @@
+#' deposits Dataset
+#'
+#' A dataset containing an OBD2 log output maintaining 40 mph for 5 minutes
+#'
+#' @format A data frame with 600 rows and 5 variables:
+#' \describe{
+#'   \item{Date}{Date formatted as MM/DD/YYYY}
+#'   \item{Category}{Transaction category}
+#'   \item{Merchant}{Vendor or merchant}
+#'   \item{Amount}{Transaction total}
+#' }
+#' @source Example dataset
+"deposits"
